94680af654d470ceaea9811fb9f1d88c36c4b232
imask333 <114331111+imask333@users.noreply.github.com>
2023-03-01 16:00:37 -0800

    Updated

--- grade.sh
+++ grade.sh
@@ -1,5 +1,23 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH=".;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar"
 
 rm -rf student-submission
 git clone $1 student-submission
+cd student-submission
+if [[ -f ListExamples.java ]]
+then 
+    echo "File found"
+else 
+    echo "Error!"
+    exit
+fi
+cp ../GradeServer.java ./
+cp ../Server.java ./
+cp ../TestListExamples.java ./
+javac -cp $CPATH *.java 
+
+if [[ $? -ne 0 ]]
+then 
+    echo "Error"
+fi
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
 echo 'Finished cloning'
